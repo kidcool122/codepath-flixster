7e0c7415c9713dd95524fbd41f7383764ac9445d
Jace <75055276+kidcool122@users.noreply.github.com>
2022-02-23 22:02:16 -0500

    Pt 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Jace Hannays on 2/23/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
